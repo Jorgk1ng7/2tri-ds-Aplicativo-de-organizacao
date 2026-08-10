6538c4f544ce167e096a0b57cce3e3718a50e3e7
sahsteskii <melanidacostaleal@gmail.com>
2026-08-10 12:18:52 -0300

    adicionando cards

---
+++ jorge/script.js
@@ -0,0 +1,46 @@
+function toggleSidebar() {
+    document.getElementById("sidebar").classList.toggle("show");
+  }
+
+  // Elementos
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.getElementById("sidebar");
+const sidebarOverlay = document.getElementById("sidebarOverlay");
+
+
+// Abrir / fechar menu
+menuBtn.addEventListener("click", function () {
+
+    sidebar.classList.toggle("show");
+    sidebarOverlay.classList.toggle("show");
+
+});
+
+
+// Fechar clicando no fundo escuro
+sidebarOverlay.addEventListener("click", function () {
+
+    sidebar.classList.remove("show");
+    sidebarOverlay.classList.remove("show");
+
+});
+
+
+// Fechar menu quando clicar em um link
+const sidebarLinks = sidebar.querySelectorAll("a");
+
+sidebarLinks.forEach(function (link) {
+
+    link.addEventListener("click", function () {
+
+        sidebar.classList.remove("show");
+        sidebarOverlay.classList.remove("show");
+
+    });
+
+});
+
+function redirecionar() {
+
+    window.location.href = 'https://example.com';
+  }
